f502f1e308ace8d11476d621d1e1a3d13d3a96ca
Soufia Shirani <118484259+soufia1101@users.noreply.github.com>
2023-12-21 15:19:21 +0330

    Add files via upload
    second unit test

--- Phonebook_test.java
+++ Phonebook_test.java
@@ -11,4 +11,11 @@ public class Phonebook_test {
         assertEquals("Jo March", person.getName());
         assertEquals("09962223344", person.getPhone());
     }
+    @Test
+    public void testSetName() {
+        Person person = new Person("Jo March", "09962223344");
+        person.setName("Jo March");
+        assertEquals("Jo March", person.getName());
+    }
+
 }
