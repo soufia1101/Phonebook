b2ca528ace2a8bb35b43f556380c4b9df3d4fb57
Soufia Shirani <118484259+soufia1101@users.noreply.github.com>
2023-12-21 16:46:49 +0330

    Add files via upload
    ...

--- Phonebook_test.java
+++ Phonebook_test.java
@@ -1,10 +1,17 @@
 import org.example.Person;
+import org.example.Phonebook;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class Phonebook_test {
+    private Phonebook phonebook;
+    private Person person1;
+    private Person person2;
+
     @Test
     public void testPersonConstructor_ValidPhone() {
         Person person = new Person("Jo March", "09962223344");
@@ -48,6 +55,27 @@ public class Phonebook_test {
         Person person2 = new Person("Person2", "09222222222");
 
         assertNotEquals(person1.getId(), person2.getId());
+    }
+    @Before
+    public void setUp() {
+        phonebook = new Phonebook();
+        // Creating persons
+        person1 = new Person("Amy", "09123456789");
+        person2 = new Person("Laurie", "09876543210");
+        // Adding persons to phonebook
+        phonebook.addContact(person1);
+        phonebook.addContact(person2);
+    }
+    @Test
+    public void addContact_ShouldAddNewContact() {
+        Person person = new Person("Charlie", "09987654321");
+        phonebook.addContact(person);
+        assertEquals(3, phonebook.getAllContacts().size());
+    }
+    @Test
+    public void getPhoneNumber_ShouldReturnPhoneNumber() {
+        String phone = phonebook.getPhoneNumber("Amy");
+        assertEquals("09123456789", phone);
     }
 
 
